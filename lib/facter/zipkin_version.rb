05ae18c4f132da8d895316f31f8b6fd4f57ff301
Martin Simon <me@martinsimon.me>
2019-11-25 12:02:58 +0100

    v0.5.1 - added custom zipkin_version fact

---
+++ lib/facter/zipkin_version.rb
@@ -0,0 +1,20 @@
+Facter.add(:zipkin_version) do
+    setcode do
+        begin
+            require 'uri'
+            require 'json'
+            require 'net/http'
+
+            uri = URI.parse("http://localhost:9411/info")
+            request = Net::HTTP::Get.new(uri)
+
+            response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+                http.request(request)
+            end
+
+            parsed_json = JSON.parse(response.body)
+            parsed_json['zipkin']['version']
+        rescue
+        end
+    end
+end
